a12fd102aa00c745de61ca0e36d0c4bb6ca33606
Artemiy <rendrom@gmail.com>
2018-12-13 19:07:27 +0800

    detail carousel (commented)

--- src/components/Detail/Detail.vue
+++ src/components/Detail/Detail.vue
@@ -2,6 +2,7 @@
   <div v-if="detail && meta">
 <!-- араж использовался как автобусный парк [1960-е, ГИГ] -->
     <div v-if="photos && photos.length">
+
       <a v-for="photo in photos" :key="photo.id" @click="dialog = true; bigImgSrc = photo.link_big">
         <v-img
           :src="photo.link_small"
@@ -10,37 +11,33 @@
         </v-img>
       </a>
 
-    <v-dialog v-model="dialog" scrollable>
-      <v-card v-if="bigImgSrc">
-        <v-card-text>
-          <div class="title grey--text" style="padding: 0 7px">
-            <v-img
-              :src="bigImgSrc"
-              class="grey lighten-2"
-            >
-            </v-img>
-          </div>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn flat @click.stop="dialog = false;">Закрыть</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+      <!-- <v-carousel>
+        <a v-for="photo in photos" :key="photo.id" @click="dialog = true; bigImgSrc = photo.link_big">
+          <v-carousel-item
+            :src="photo.link_small"
+            class="grey lighten-2"
+          >
+          </v-carousel-item>
+        </a>
+      </v-carousel> -->
 
-      <!-- <v-dialog  v-if="bigImgSrc" v-model="dialog">
-        <v-layout>
-          <v-flex>
-              <a @click="dialog = false;">
-                <v-img
-                  :src="bigImgSrc"
-                  class="grey lighten-2"
-                >
-                </v-img>
-              </a>
-          </v-flex>
-        </v-layout>
-      </v-dialog> -->
+      <v-dialog v-model="dialog" scrollable>
+        <v-card v-if="bigImgSrc">
+          <v-card-text>
+            <div class="title grey--text" style="padding: 0 7px">
+              <v-img
+                :src="bigImgSrc"
+                class="grey lighten-2"
+              >
+              </v-img>
+            </div>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn flat @click.stop="dialog = false;">Закрыть</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </div>
 
     <v-list dense>
